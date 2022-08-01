e4a01ce8240e4fd0f1a777dafc7139bcb9d588e3
Steve Witman <stevewitman@gmail.com>
2022-08-01 11:01:23 -0600

    new proj w/ angular/material, angular/fire, home comp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'crud-65f97',
+    appId: '1:950880780939:web:df8658ba1eb52c501a0a8e',
+    storageBucket: 'crud-65f97.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyA01BeSmm35zHPlyGA6-H4x4_fqhlOq2ig',
+    authDomain: 'crud-65f97.firebaseapp.com',
+    messagingSenderId: '950880780939',
+    measurementId: 'G-N95EC6PFET',
+  },
   production: true
 };
